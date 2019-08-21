425b29903e9b2965361160deaf21881e260e6184
Sarataj Sultan <saratajsultan@gmail.com>
2019-08-21 07:34:41 +0600

    Version 3
    Version 3 student & parent, admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,17 +5,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(App\User::class,10)->create();
-        factory(App\Course::class,10)->create();
-        factory(App\Student::class,10)->create();
-        factory(App\Faculty::class,10)->create();
-
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,53 +1,70 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+//logout
+Route::get('/logout','AdminController@logout');
+Route::get('/student_logout','AdminController@student_logout');
 
 Route::get('/', function () {
-
-    return view('welcome');
+    return view('student_login');
 });
 
-Auth::routes();
+Route::get('/admin', function () {
+    return view('admin.admin_login');
+});
 
-Route::get('/home', 'StudentController@index')->name('home');
+//Admin controller
+Route::post('/adminlogin','AdminController@login_dashboard');
+Route::post('/studentlogin','AdminController@student_login_dashboard');
 
-// student route
+//result upload
+Route::get('/result','AdminController@result');
+Route::post('/save_result','AdminController@saveresult');
 
-Route::get('/create', 'StudentController@create')->name('create');
-Route::post('/create', 'StudentController@store')->name('store');
-Route::get('/studentlist', 'StudentController@studentlist')->name('studentlist');
-Route::get('/edit', 'StudentController@edit')->name('studentlist');
-Route::get('edit/{id}', 'StudentController@editdelete')->name('editdelete');
-Route::post('update/{id}', 'StudentController@update')->name('update');
-Route::delete('delete/{id}', 'StudentController@delete')->name('delete');
-
-// student route ends
+//All Results view
+Route::get('/allresults','AdminController@allresults');
+//edit result
+Route::get('/result_edit/{result_id}','AdminController@resultedit');
+//update Result
+Route::post('/update_result/{result_id}','AdminController@resultupdate');
 
 
-// facullty route
-Route::get('/create_faculty', 'FacultyController@create_faculty')->name('create_faculty');
-Route::post('/create_faculty', 'FacultyController@store_faculty')->name('store_faculty');
-Route::get('/facultylist', 'FacultyController@facultylist')->name('facultylist');
-Route::get('/edit_faculty', 'FacultyController@edit_faculty')->name('facultylist');
-Route::get('edit_faculty/{id}', 'FacultyController@edit2_faculty')->name('edit2_faculty');
-Route::post('update_faculty/{id}', 'FacultyController@update_faculty')->name('update_faculty');
-Route::delete('delete_faculty/{id}', 'FacultyController@delete_faculty')->name('delete_faculty');
 
-// faculty route ends
 
-Route::get('/create_course', 'CourseController@create_course')->name('create_course');
-Route::post('/create_course', 'CourseController@store_course')->name('store_course');
-Route::get('/courselist', 'CourseController@courselist')->name('courselist');
-Route::get('/edit_course', 'CourseController@edit_course')->name('courselist');
-Route::get('edit_course/{id}', 'CourseController@edit2_course')->name('edit2_course');
-Route::post('update_course/{id}', 'CourseController@update_course')->name('update_course');
-Route::delete('delete_course/{id}', 'CourseController@delete_course')->name('delete_course');
+
+Route::get('/student_dashboard','AdminController@student_dashboard');
+Route::get('/adminprofile','AdminController@adminprofile');
+
+Route::get('/admin_dashboard','AdminController@admin_dashboard');
+
+
+Route::get('/student_result','AdminController@studentresult');
+
+//Route::get('/viewprofile','AdminController@viewprofile');
+Route::get('/setting','AdminController@setting');
+Route::get('/student_setting','AdminController@studentsetting');
+
+//student profile
+Route::get('/student_profile','AddstudentsController@studentprofile');
+Route::get('/dashboard','AddstudentsController@studentprofile_2');
+
+
+//addStudent
+Route::get('/addstudent','AddstudentsController@addstudent');
+Route::post('/save_student','AddstudentsController@savestudent');
+
+//viewStudent
+Route::get('/student_view/{student_id}','AllstudentsController@studentview');
+
+//editStudent
+Route::get('/student_edit/{student_id}','AllstudentsController@studentedit');
+//update student(admin)
+Route::post('/update_student/{student_id}','AllstudentsController@studentupdate');
+//update student(student own)
+Route::post('/student_own_update','AllstudentsController@studentownupdate');
+
+//deleteStudent
+Route::get('/student_delete/{student_id}','AllstudentsController@studentdelete');
+
+//allstudent
+Route::get('/allstudent','AllstudentsController@allstudent');
